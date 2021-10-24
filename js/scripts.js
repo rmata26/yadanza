f60ed4b403db060935ab79393fdd19cd9a61653e
Ronny <rmata@itcoint.com>
2021-10-24 00:21:59 -0600

    Agregar Contactenos LandingPage

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,22 @@ function playAudio(){
     audioElement[0].play();
 
 }
+
+$(function() {
+	$("#feedback-tab").click(function() {
+		$("#feedback-form").toggle("slide");
+	});
+
+	$("#feedback-form form").on('submit', function(event) {
+		var $form = $(this);
+		$.ajax({
+			type: $form.attr('method'),
+			url: $form.attr('action'),
+			data: $form.serialize(),
+			success: function() {
+				$("#feedback-form").toggle("slide").find("textarea").val('');
+			}
+		});
+		event.preventDefault();
+	});
+});
